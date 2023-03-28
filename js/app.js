ae247328324c03cfbd783394063f7463af25fcfb
ahkamulbd <ahkamulbd@gmail.com>
2023-03-28 14:45:12 +0600

    Phone Hunter API data presentation on UI initiated with Bootstrap

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const loadPhones = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
+    //console.log(data.data);
+    displayPhones(data.data);
+}
+
+const displayPhones = phones => {
+    console.log(phones);
+}
+
+
+loadPhones();
